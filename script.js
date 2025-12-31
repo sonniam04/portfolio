1ac74766de9840fdcbdbba0f488440f50251e43e
JessadaJay <152261754+JessadaJay@users.noreply.github.com>
2025-12-31 13:17:37 +0700

    first product

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 function menubar() {
-    let nav = document.getElementById("nav")
-
+    let nav = document.getElementById("navigation-bar")
     nav.classList.toggle("toggle")
 }
+function scrollToAbout(section) {
+    let nav = document.getElementById("navigation-bar")
+    nav.classList.toggle("toggle")
+    const view = document.getElementById(section);
+    view.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+
+    
+}
+function initSlider() {
+  let current = 0;
+  const slides = document.querySelectorAll('.slide');
+  const total = slides.length;
+    console.log(slides)
+  function moveSlide(direction) {
+    slides.forEach(slide => slide.classList.remove("active"));
+
+    current = (current + direction + total) % total;
+
+    slides[current].classList.add("active");
+  }
+
+  document.querySelector(".next").onclick = () => moveSlide(1);
+  document.querySelector(".prev").onclick = () => moveSlide(-1);
+}
+
+initSlider();
